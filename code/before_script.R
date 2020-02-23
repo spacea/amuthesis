e54bcd9cfb6dcc050cafce3a38fa703c5298f05e
Nowosad <tupiszakaczki@gmail.com>
2020-02-23 08:38:23 +0100

    adds before script

---
+++ code/before_script.R
@@ -0,0 +1,13 @@
+knitr::opts_chunk$set(
+  background = "#FCFCFC", # code chunk color in latex
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  fig.pos = "t",
+  fig.path = "figures/",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold",
+  out.width = "100%"
+)
